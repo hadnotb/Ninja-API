17d4e2703d6a3b30006a890f55a293aa4332fcf3
hadnotb <adnot.hichem@gmail.comgit config --global user.email adnot.hichem@gmail.com>
2021-11-04 09:55:04 +0100

    ajout

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php 
+
+// Configuration des chemins vers les répertoires
+define('PROJECT_DIR', __DIR__);
+define('CONTROLLER_DIR', PROJECT_DIR . '/controller');
+define('TEMPLATE_DIR', PROJECT_DIR . '/templates');
+define('RSX_DIR', PROJECT_DIR . '/public/rsx');
+define('MAP_DIR', RSX_DIR . '/map');
+
+// Configuration des accès à la base de données
+define('DB_HOST', '');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
+define('DB_PORT', '');
+
+define('SITE_BASE_URL', 'http://localhost:8000');
